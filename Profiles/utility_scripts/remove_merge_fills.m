0cf979830480bf54cac241e06a81a9030a92f4a6
Josh Laughner <joshlaugh5@gmail.com>
2014-07-31 15:25:29 -0700

    Averaging pixels to profiles is working okay. Preparing to branch to try a better way of determining the altitude to integrate from.

--- Profiles/utility_scripts/remove_merge_fills.m
+++ Profiles/utility_scripts/remove_merge_fills.m
@@ -1,4 +1,5 @@
-function [ data, utc, alt, lon, lat, fills ] = remove_merge_fills( Merge, field, varargin )
+%function [ data, utc, alt, lon, lat, fills ] = remove_merge_fills( Merge, field, varargin )
+function [ data, varargout ] = remove_merge_fills( Merge, field, varargin )
 %remove_merge_fills(Merge, field) Returns "field", utc, alt, lat, lon (properly
 %scaled) with fill values in "field" replaced with NaNs.  Also returns the
 %logical matrix of which entries were fills as the fifth output.  If you
@@ -23,18 +24,41 @@ lonfield = pout.lon;
 latfield = pout.lat;
 
 data = eval(sprintf('Merge.Data.%s.Values',field));
-utc = eval(sprintf('Merge.Data.%s.Values',utcfield));
-alt = eval(sprintf('Merge.Data.%s.Values',altfield));
-lat = eval(sprintf('Merge.Data.%s.Values',latfield));
-lon = eval(sprintf('Merge.Data.%s.Values',lonfield));
-lon(lon>180) = lon(lon>180) - 360;
-
 fill_val = eval(sprintf('Merge.Data.%s.Fill',field));
 ulod = Merge.metadata.upper_lod_flag;
 llod = Merge.metadata.lower_lod_flag;
 
 fills = data == fill_val | data == ulod | data == llod;
 data(fills) = NaN;
+if nargout > 5; varargout{5} = fills; end
+
+if nargout > 1; 
+    utc = eval(sprintf('Merge.Data.%s.Values',utcfield)); 
+    utcfills = eval(sprintf('Merge.Data.%s.Fill',utcfield));
+    utc(utc==utcfills) = NaN;
+    varargout{1} = utc;
+end
+if nargout > 2; 
+    alt = eval(sprintf('Merge.Data.%s.Values',altfield)); 
+    altfills = eval(sprintf('Merge.Data.%s.Fill',altfield));
+    alt(alt==altfills) = NaN;
+    varargout{2} = alt;
+end
+if nargout > 4; 
+    lat = eval(sprintf('Merge.Data.%s.Values',latfield)); 
+    latfills = eval(sprintf('Merge.Data.%s.Fill',latfield));
+    lat(lat==latfills) = NaN;
+    varargout{4} = lat;
+end
+if nargout > 3; 
+    lon = eval(sprintf('Merge.Data.%s.Values',lonfield)); 
+    lonfills = eval(sprintf('Merge.Data.%s.Fill',lonfield));
+    lon(lon==lonfills) = NaN;
+    lon(lon>180) = lon(lon>180) - 360;
+    varargout{3} = lon;
+end
+
+
 
 end
 
